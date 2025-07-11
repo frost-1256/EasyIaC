c8d1bd219cd36167c83a5a60808ac2d93ba198eb
spring <habanero.iori@gmail.com>
2025-07-11 18:06:12 +0900

    Added Args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
-    println!("Test!");
+    let args: Vec<String> = args().collect();
+    if args.len() > 1 {
+        println!("args found");
+    } else {
+        println!("args not found")
+    }
 }
